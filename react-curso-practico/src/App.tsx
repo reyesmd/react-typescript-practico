5b01c4acedeee38ad3c28ab7eb2834e8daac0e0c
reyesmd <reyes.madu@gmail.com>
2026-05-18 09:24:37 +0000

    feat: proyecto base vite react-ts

---
+++ react-curso-practico/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>React + TypeScript + Vite</h1>
+      <p>Si ves este texto, el lab 2.1 está correcto.</p>
+      <p>Bienvenida Reyes!</p>
+    </div>
+  );
+}
+
+export default App;
